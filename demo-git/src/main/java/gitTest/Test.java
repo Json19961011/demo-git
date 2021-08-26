e35d2cbc32766808f36a92719b9b536b8a60ebe8
huangkailiang <2048519916@qq.com>
2021-08-26 19:15:18 +0800

    master commit

---
+++ demo-git/src/main/java/gitTest/Test.java
@@ -0,0 +1,13 @@
+package gitTest;
+
+/**
+ * @Author:huang
+ * @Date: Created in 2021/8/26 17:32
+ * @Description：
+ * @Version:1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试Git提交");
+    }
+}
