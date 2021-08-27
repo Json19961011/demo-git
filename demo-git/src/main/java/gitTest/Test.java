8027fb5b2621fd8a36271e31105ac4b36dd7bed8
Json19961011 <74534781+Json19961011@users.noreply.github.com>
2021-08-27 14:23:28 +0800

    pull commit
    pull commit

--- demo-git/src/main/java/gitTest/Test.java
+++ demo-git/src/main/java/gitTest/Test.java
@@ -12,5 +12,6 @@ public class Test {
         System.out.println("master测试Git提交");
         System.out.println("hot-fix测试Git提交");
         System.out.println("master向GitHub推送新的代码");
+        System.out.println("master从GitHub拉去新的代码");
     }
 }
